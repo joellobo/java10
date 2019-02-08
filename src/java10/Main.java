fa449e8735a603c757767b457820c77c0790e3fa
Joel Lobo <joellobo@gmail.com>
2019-02-07 22:21:39 -0300

    add wiki

--- src/java10/Main.java
+++ src/java10/Main.java
@@ -1,7 +1,7 @@
 package java10;
 
 /**
- * Reference:
+ * https://en.wikipedia.org/wiki/Java_version_history
  * https://www.oracle.com/technetwork/java/javase/10-relnote-issues-4108729.html
  * https://www.techworld.com/developers/java-10-features-whats-new-in-java-10-3680317/
  * @author joellobo
