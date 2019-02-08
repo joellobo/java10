73aa721995c604a08372b8960ceb72e519912b4f
Joel Lobo <joellobo@gmail.com>
2019-02-07 22:14:30 -0300

    initial commit

---
+++ src/java10/Main.java
@@ -0,0 +1,16 @@
+package java10;
+
+/**
+ * Reference:
+ * https://www.techworld.com/developers/java-10-features-whats-new-in-java-10-3680317/
+ * @author joellobo
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
